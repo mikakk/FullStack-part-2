362526f0a1085527c09af046bc50f6dea09f0bd8
Mika Kärkkäinen <mika.karkkainen@koivuniemi.com>
2018-10-18 18:10:57 +0300

    3.11

--- services/persons.js
+++ services/persons.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 /*const baseUrl = "http://localhost:3001/api/persons";*/
-const baseUrl = "https://fullstack-part-3.now.sh/api/persons";
+/*const baseUrl = "https://fullstack-part-3.now.sh/api/persons";*/
+const baseUrl = "/api/persons";
 const getAll = () => {
     const request = axios.get(baseUrl);
     return request.then(response => response.data);
